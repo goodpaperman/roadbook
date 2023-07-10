719651eabd7609c260f3a977c46fdca41e7d3174
yunh <haihai107@126.com>
2023-07-10 19:47:11 +0800

    batch convert gps coordinate to baidu

--- gps2bd.sh
+++ gps2bd.sh
@@ -5,29 +5,66 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
+# @brief: is MacOS platform
+#         mac date a little different with other
+# @retval: 0 - no
+# @retval: 1 - yes
+function is_macos()
+{
+    local os="${OSTYPE/"darwin"//}"
+    if [ "$os" != "$OSTYPE" ]; then
+        # darwin: macos
+        return 1
+    else
+        return 0
+    fi
+}
+
+is_macos
+IS_MAC=$?
+
 BAIDU_MAP_AK="rNGtgGPYWcApvwpOXAMsND96iMZgYrL0"
 file="$1"
+end=0
+n=0
 line=""
+data=""
 
-while :
+while [ ${end} -eq 0 ]; 
 do
-    read -r line
-    if [ $? -ne 0 -a -z "${line}" ]; then 
-        # last line without LF will trigger read return error
-        # so here check content of 'line', too
-        break; 
+    while [ $n -lt 100 ];
+    do
+        read -r line
+        if [ $? -ne 0 -a -z "${line}" ]; then 
+            # last line without LF will trigger read return error
+            # so here check content of 'line', too
+            end=1
+            break; 
+        fi
+
+        # delete spaces
+        # line="${line// /}"
+        # replace space to ','
+        line="${line/ /,}"
+        # echo "${line}"
+        if [ $n -eq 0 ]; then 
+            data="${line}"
+        else
+            data="${data};${line}"
+        fi
+
+        n=$((n+1))
+    done
+
+    if [ $n -gt 0 ]; then 
+        resp=$(curl -gs "https://api.map.baidu.com/geoconv/v1/?coords=${data}&from=1&to=5&ak=${BAIDU_MAP_AK}")
+        # echo "$n: ${resp}"
+        if [ ${IS_MAC} -eq 0 ]; then 
+            echo "${resp}" | jq -r '.result[]|.x,.y' | sed -n '{N;s/\n/ /p}' | awk '{printf "\"%s,%s\",\n",$1,$2}'
+        else
+            echo "${resp}" | jq -r '.result[]|.x,.y' | gsed -n '{N;s/\n/ /p}' | awk '{printf "\"%s,%s\",\n",$1,$2}'
+        fi
     fi
 
-    # delete spaces
-    # line="${line// /}"
-    # replace space to ','
-    line="${line/ /,}"
-    # echo "${line}"
-    resp=$(curl -gs "https://api.map.baidu.com/geoconv/v1/?coords=${line}&from=1&to=5&ak=${BAIDU_MAP_AK}")
-    # echo "${resp}"
-    if [ ! -z "${resp}" ]; then 
-        x=$(echo "${resp}" | jq '.result[0].x')
-        y=$(echo "${resp}" | jq '.result[0].y')
-        echo "\"$x,$y\","
-    fi
+    n=0
 done < "${file}"
